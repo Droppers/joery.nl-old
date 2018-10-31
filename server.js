c5fc9dce86822b48ff7515462c9b1e5fdfe1e939
Joery <joery@droppe.rs>
2018-10-31 20:57:16 +0100

    Minor fixes (again)

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
+var express = require('express');
+var app = express();
 var methodOverride = require('method-override');
 
-var port = process.env.PORT || 8080; // set our port
-var staticdir = process.env.NODE_ENV === 'production' ? 'dist.prod' : 'dist.dev'; // get static files dir
+var port = process.env.PORT || 8080;
+var staticdir = process.env.NODE_ENV === 'production' ? 'dist.prod' : 'dist.dev';
 
-// get all data/stuff of the body (POST) parameters
-app.use(bodyParser.json()); // parse application/json
-//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-//app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use('/assets', express.static(__dirname + '/' + staticdir)); // set the static files location /public/img will be /img for users
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use('/assets', express.static(__dirname + '/' + staticdir));
 
 app.get('/*', function(req, res) {
 	if(req.path.indexOf('.') === -1)
@@ -21,10 +15,6 @@ app.get('/*', function(req, res) {
 		res.sendFile(__dirname + '/'+staticdir+'/' + req.path)
 });
 
-// routes ==================================================
-//require('./dev/routes')(app); // configure our routes
-
-// start app ===============================================
-app.listen(port);                   // startup our app at http://localhost:8080
-console.log('Starting sever on port ' + port);       // shoutout to the user
-exports = module.exports = app;             // expose app
+app.listen(port);
+console.log('Starting sever on port ' + port);
+exports = module.exports = app;
